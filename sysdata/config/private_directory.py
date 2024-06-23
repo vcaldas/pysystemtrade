2fe677c3792ed6f4f2abd97cf2be49d8873dc1be
Victor Caldas <caldas.victor@gmail.com>
2024-06-23 23:08:21 +0200

    Changes access to private data

--- sysdata/config/private_directory.py
+++ sysdata/config/private_directory.py
@@ -1,5 +1,6 @@
 import os
 
+from sysdata import BASE_PATH
 DEFAULT_PRIVATE_DIR = "private"
 PRIVATE_CONFIG_DIR_ENV_VAR = "PYSYS_PRIVATE_CONFIG_DIR"
 
@@ -10,6 +11,6 @@ def get_full_path_for_private_config(filename: str):
     if os.getenv(PRIVATE_CONFIG_DIR_ENV_VAR):
         private_config_path = f"{os.environ[PRIVATE_CONFIG_DIR_ENV_VAR]}/{filename}"
     else:
-        private_config_path = f"{DEFAULT_PRIVATE_DIR}/{filename}"
+        private_config_path = os.path.join(BASE_PATH, DEFAULT_PRIVATE_DIR, filename)
 
     return private_config_path
