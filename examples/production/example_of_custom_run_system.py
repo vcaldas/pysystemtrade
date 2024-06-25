a7e8558ab68c167865fa94a2fe87055b22976af7
Victor Caldas <caldas.victor@gmail.com>
2024-06-25 22:17:48 +0200

    Fix imports

--- examples/production/example_of_custom_run_system.py
+++ examples/production/example_of_custom_run_system.py
@@ -60,8 +60,8 @@ from sysdata.config.configdata import Config
 from systems.forecasting import Rules
 from systems.basesystem import System
 from systems.forecast_combine import ForecastCombine
-from private.systems.arch.carrytrend.forecastScaleCap import volAttenForecastScaleCap
-from private.systems.arch.carrytrend import myFuturesRawData
+from systems.provided.attenuate_vol.vol_attenuation_forecast_scale_cap import volAttenForecastScaleCap
+from systems.provided.rob_system.rawdata import myFuturesRawData
 from systems.positionsizing import PositionSizing
 from systems.portfolio import Portfolios
 from systems.provided.dynamic_small_system_optimise.optimised_positions_stage import (
